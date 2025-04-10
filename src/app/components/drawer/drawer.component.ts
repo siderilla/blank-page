2c6e70c7467a33bd4305d2cf064f4eb368f8a12e
halex22 <halex2209@gmail.com>
2025-04-10 12:18:36 +0200

    init commit with project structure

---
+++ src/app/components/drawer/drawer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drawer',
+  imports: [],
+  templateUrl: './drawer.component.html',
+  styleUrl: './drawer.component.scss'
+})
+export class DrawerComponent {
+
+}
